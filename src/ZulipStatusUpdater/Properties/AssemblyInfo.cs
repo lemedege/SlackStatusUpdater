9e7d4dbceb8548aa478d5c38c2133f6a6ddcacd1
lemedege <lassegyrn@gmail.com>
2023-09-07 13:27:23 +0200

    Bumped version to 0.3.0

--- src/ZulipStatusUpdater/Properties/AssemblyInfo.cs
+++ src/ZulipStatusUpdater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ZulipStatusUpdater")]
-[assembly: AssemblyCopyright("Copyright © Jerri Rantio 2018 - Modified for Zulip")]
+[assembly: AssemblyCopyright("Copyright © Jerri Rantio 2018 - Modified for Zulip by LG")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.6.1")]
-[assembly: AssemblyFileVersion("0.2.6.1")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
